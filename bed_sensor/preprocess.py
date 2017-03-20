f425a4e1660e3669e4d7f0f51c6f248608f3963c
Kasperi Palkama <kasperi.palkama@gmail.com>
2017-03-20 21:06:25 +0200

    add bedOccupied and replaceZeros functions

--- bed_sensor/preprocess.py
+++ bed_sensor/preprocess.py
@@ -20,6 +20,7 @@ from matplotlib import style,pyplot as plt
 import numpy as np
 from scipy.signal import find_peaks_cwt, medfilt
 
+
 def preprocess_hr(df):
     df.hr = medfilt(df.hr,299)
 #    df.hr = pd.rolling_mean(df.hr,500)
@@ -29,6 +30,22 @@ def preprocess_rr(df):
     df.rr = medfilt(df.rr,299)
     return df
 
+def bedOccupied(df):
+    '''
+    cuts signal when subject goes to bed and gets up
+    '''
+    df.status = medfilt(df.status,299)
+    occupyedStartIdx = df.status[df.status == 1].index[0]
+    occupyedEndIdx = df.status[df.status == 1].index[-1]
+    return df.iloc[occupyedStartIdx:occupyedEndIdx+1,:]
+
+def replaceZeros(df):
+    '''
+    replace zeros with preceeding value
+    '''
+    df.hr = df.hr.replace(to_replace=0, method='ffill')
+    df.rr = df.rr.replace(to_replace=0,method='ffill')
+    return df
     
 def count_peaks(var,width):
     peak_list = find_peaks_cwt(var, np.arange(1,width))
@@ -48,9 +65,12 @@ if __name__ == "__main__":
     df = pd.read_csv('/Users/kasperipalkama/Downloads/48223c_MurataBSN_'\
                      'MurataBSN_2017-03-19-23-00-00-2017-03-20-08-30-00.csv')
     df = preprocess_time(df)
+    
     new_df = df.copy()
     new_df = preprocess_hr(new_df)
     new_df = preprocess_rr(new_df)
+    new_df = bedOccupied(new_df)
+    new_df = replaceZeros(new_df)
 #    hr_peaks = np.array(count_peaks(new_df['hr'],width=3600))
 #    rr_peaks = np.array(count_peaks(new_df['rr'],width=3600))
     for var,leg in zip(['hr','rr'],[['hr','hr filtered'],['rr','rr filtered']],):
